3cb74fc60dc9f13b47921281ca569f5d43ed3727
unknown <chanukyasai08@gmail.com>
2024-07-25 15:25:20 +0530

    my project notes

---
+++ react-workspace/react_frontend/src/components/FooterComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FooterComponent() {
+  return (
+    <footer className='footer'>
+        <span>All Right Reserved 2024 &copy;CHANUKYA_SAI</span>
+    </footer>
+  )
+}
